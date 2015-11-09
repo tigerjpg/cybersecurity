bb89aae02f4fa0a7423997d1b07a818a58c33736
jesse <jessemazzella@gmail.com>
2015-11-08 17:58:56 -0800

    [#22] insert into database works, changes are persistent

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <QSqlDatabase>
 #include <QSqlQuery>
 #include <QDebug>
+#include <QException>
 
 int main(int argc, char *argv[])
 {
@@ -13,17 +14,45 @@ int main(int argc, char *argv[])
   db->setHostName("localhost");
   db->setDatabaseName("../data/data.db");
 
+  //Open database
   Q_ASSERT(db->open());
   QSqlQuery query;
+  //Get all info from table customers
   Q_ASSERT(query.exec("select * from customers;"));
   while(query.next())
   {
-    QString name = query.value(0).toString();
-    QString address = query.value(1).toString();
-    QString interest = query.value(2).toString();
-    bool isKey = query.value(3).toBool();
-    qDebug() << name << ' ' << address << ' ' << interest << ' ' << isKey;
+    int customerId = query.value("id").toInt();
+    QString name = query.value("name").toString();
+    QString address = query.value("address").toString();
+    int interest = query.value("interest").toInt();
+    bool isKey = query.value("key").toBool();
+    qDebug() << customerId << name << ' ' << address << ' ' << interest << ' ' << isKey;
   }
+
+  Q_ASSERT(query.exec("select * from admins"));
+  while(query.next())
+  {
+    QString name = query.value(0).toString();
+    QString pass = query.value(1).toString();
+    qDebug() << name << ' ' << pass;
+  }
+
+  //add to database
+  Q_ASSERT(query.exec("insert into customers values (NULL, \"jesse\", \"7 hazelbranch\", 2, 1);"));
+
+  //print database
+  Q_ASSERT(query.exec("select * from customers;"));
+  while(query.next())
+  {
+    int customerId = query.value("id").toInt();
+    QString name = query.value("name").toString();
+    QString address = query.value("address").toString();
+    int interest = query.value("interest").toInt();
+    bool isKey = query.value("key").toBool();
+    qDebug() << customerId << name << ' ' << address << ' ' << interest << ' ' << isKey;
+  }
+
+
   w.show();
 
   return a.exec();

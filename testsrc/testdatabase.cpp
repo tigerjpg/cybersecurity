98fad5b11284fcbf074dfc066e4c0946caaeefd2
jesse <jessemazzella@gmail.com>
2015-11-16 18:12:35 -0800

    [#35] add comments to TestDatabase

--- testsrc/testdatabase.cpp
+++ testsrc/testdatabase.cpp
@@ -1,10 +1,15 @@
 #include <QtTest/QtTest>
 #include "database.h"
+/*!
+ * \brief The TestDatabase class is a unit test for the Database class
+ */
 class TestDatabase : public QObject
 {
   Q_OBJECT
 public:
+  ///Constructor
   TestDatabase();
+  ///Destructor
   ~TestDatabase();
 private slots:
   void TestOpen();
@@ -19,19 +24,29 @@ private:
 
 TestDatabase::TestDatabase()
 {
+  //Initialize Database object
   db = new Database("../cybersecurity/data/data.db", "QSQLITE");
 }
 
 TestDatabase::~TestDatabase()
 {
+  //Close database
   db->close();
 }
 
+/*!
+ * \brief TestDatabase::TestOpen
+ * Verify that the Database file is successfully opened.
+ */
 void TestDatabase::TestOpen()
 {
   QVERIFY(db->isOpen());
 }
 
+/*!
+ * \brief TestDatabase::TestTables
+ * Tests that the tables exist in the SQL database
+ */
 void TestDatabase::TestTables()
 {
   QVERIFY(db->SetQuery("select * from customers"));
@@ -44,6 +59,11 @@ void TestDatabase::TestTables()
   QVERIFY(db->Exec());
 }
 
+/*!
+ * \brief TestDatabase::TestAddCustomers
+ * Test the AddCustomer method by adding a valid test customer,
+ * removing it, and attempting to add an invalid test customer.
+ */
 void TestDatabase::TestAddCustomers()
 {
   try
@@ -67,11 +87,20 @@ void TestDatabase::TestAddCustomers()
   }
 }
 
+/*!
+ * \brief TestDatabase::TestRemoveCustomers
+ * Test the RemoveCustomer method by removing the test customer
+ * added in the previous test.
+ */
 void TestDatabase::TestRemoveCustomers()
 {
   QVERIFY(db->RemoveCustomer("TEST"));
 }
 
+/*!
+ * \brief TestDatabase::TestIsKey
+ * Test the IsKey method by checking true and false conditions.
+ */
 void TestDatabase::TestIsKey()
 {
   try
